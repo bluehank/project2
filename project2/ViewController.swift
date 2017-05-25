6465acd1207079481e9bb855df92dd50638e7831
Henry Lourtie <henry.lourtie@gmail.com>
2017-05-25 15:46:31 +0200

    My first commit

--- project2/ViewController.swift
+++ project2/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLabel: UILabel!
+    var te = "hello world"
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        if te == "hello world"{
+        coolLabel.text = "goodbye world"
+        te = "goodbye world"
+        }
+            
+        else{
+            te = "hello world"
+        coolLabel.text = "hello world"
+        }
+    }
+    @IBAction func buttontipped(_ sender: Any) {
+        coolLabel.text = "Buttons are cool"
+        te = "Buttons are cool"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
